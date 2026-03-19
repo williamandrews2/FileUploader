814429924be8513f64488ded192643d5b5e70fe1
William A. <121400607+williamandrews2@users.noreply.github.com>
2026-03-18 21:45:06 -0500

    Created the dashboard controller.

---
+++ controllers/dashboardController.js
@@ -0,0 +1,13 @@
+exports.dashboardGet = async (req, res) => {
+  // grab all the folder to show on the dashboard
+  const folders = await prisma.folder.findMany({
+    where: { userId: req.user.id },
+  });
+
+  // grab all the files that are in the root directory to show on the dashboard
+  const files = await prisma.file.findMany({
+    where: { userId: req.user.id, folderId: null },
+  });
+
+  res.render("dashboard", { title: "dashboard", folders, files });
+};
