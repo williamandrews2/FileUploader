4cc85e57732683c5b4efbb94655b79e6d1ba91ea
William A. <121400607+williamandrews2@users.noreply.github.com>
2026-03-21 23:17:45 -0500

    Fixed event bubbling issue with delete buttons on dashboard.

--- public/js/confirmDelete.js
+++ public/js/confirmDelete.js
@@ -1,5 +1,6 @@
 document.querySelectorAll(".delete-form").forEach(function (form) {
   form.addEventListener("submit", (e) => {
+    e.stopPropagation();
     const confirmed = confirm(
       "Are you sure you want to delete this? You cannot undo this action!",
     );
